e93409b8716e16176d76163424cdc0edefcc791c
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-05-05 23:21:54 +0800

    Create harbour-qrclip-zh_CN.ts

---
+++ translations/harbour-qrclip-zh_CN.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name></name>
+    <message id="qrclip-app_name">
+        <source>QR Clip</source>
+        <extracomment>Application title (rarely needs to be translated)</extracomment>
+        <translation>二维码剪切</translation>
+    </message>
+    <message id="qrclip-placeholder-no_text">
+        <source>No text in clipboard.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>剪切板内无文本。</translation>
+    </message>
+    <message id="qrclip-placeholder-text_too_long">
+        <source>Text in clipboard is too long for QR code.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>剪切板文本太长，无法生成二维码。</translation>
+    </message>
+</context>
+</TS>
