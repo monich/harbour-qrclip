8fa78a43ab3c3b5781d07316373feb1936adef5b
Åke Engelbrektson <eson57@users.noreply.github.com>
2019-05-05 10:25:41 +0200

    Create harbour-qrclip-sv.ts
    Swedish translation if you want.

---
+++ translations/harbour-qrclip-sv.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="qrclip-app_name">
+        <source>QR Clip</source>
+        <extracomment>Application title (rarely needs to be translated)</extracomment>
+        <translation>QR Clip</translation>
+    </message>
+    <message id="qrclip-placeholder-no_text">
+        <source>No text in clipboard.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Det finns ingen text i urklipp.</translation>
+    </message>
+    <message id="qrclip-placeholder-text_too_long">
+        <source>Text in clipboard is too long for QR code.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Texten i urklipp är för lång för QR-kod.</translation>
+    </message>
+</context>
+</TS>
