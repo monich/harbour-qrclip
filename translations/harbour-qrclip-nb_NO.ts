0ce02b25b5ef99642a7f1d72d7bb0b20b3aa8dc7
Slava Monich <slava@monich.com>
2026-05-15 05:40:32 +0300

    [translations] Norwegian bokmål translation

---
+++ translations/harbour-qrclip-nb_NO.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name></name>
+    <message id="qrclip-app_name">
+        <source>QR Clip</source>
+        <extracomment>Application title (rarely needs to be translated)</extracomment>
+        <translation>QR Clip</translation>
+    </message>
+    <message id="qrclip-placeholder-no_text">
+        <source>No text in clipboard.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Ingen tekst i utklippstavlen.</translation>
+    </message>
+    <message id="qrclip-placeholder-text_too_long">
+        <source>Text in clipboard is too long for QR code.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Teksten i utklippstavlen er for lang for en QR-kode.</translation>
+    </message>
+    <message id="qrclip-menu-save_to_gallery">
+        <source>Save to Gallery</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Lagre i Galleri</translation>
+    </message>
+    <message id="qrclip-menu-level">
+        <source>Level %1</source>
+        <extracomment>Pulley menu label</extracomment>
+        <translation>Nivå %1</translation>
+    </message>
+</context>
+</TS>
